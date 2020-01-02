0f6889a71294cf609968a09512e90f34b5f6de20
dongergan@163.com <dongergan@163.com>
2020-01-02 11:42:23 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <Header id="header"></Header>
+    <main id="main">
+      <router-view/>
+    </main>
+    <Footer id="footer"></Footer>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+export default {
+  name: 'App',
+  components:{
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+@import "./assets/common.less";
+#app {
+ display: grid;
+ grid-template-columns: 12% auto 12%;
+ grid-template-rows: auto 1fr auto;
+ grid-template-areas: "header header header"
+                      ".      main   ."
+                      "footer footer footer"
+}
+#header {
+  grid-area: header;
+  padding-left: 12%;
+  padding-right: 12%;
+}
+#main {
+  grid-area: main;
+}
+#footer {
+  grid-area: footer;
+  padding-left: 12%;
+  padding-right: 12%;
+}
+</style>
