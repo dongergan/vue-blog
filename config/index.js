75531df95a1bd15e694d55509a1daeb992256b30
dongergan@163.com <dongergan@163.com>
2020-01-02 11:49:04 +0800

    end

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/blog/dist',
+    assetsPublicPath: './dist',
 
     /**
      * Source Maps
